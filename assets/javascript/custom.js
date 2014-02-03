564a57e5e5cc277ae6c50063e93f5e328c4ed445
Jeff Quinn <jeff.quinn@gmail.com>
2014-02-03 14:13:32 -0500

    Change of plans - scroll to things displayed

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -1,29 +1,38 @@
 $(document).ready(function() { 
     var section=window.location.hash.substring(1);
+//    $(".starthidden").hide();
     if(section !="")
     {
         showSection($("#"+section).closest("section").attr("id"));
     }
-    $(".starthidden").hide();
     $(".mynav").on("click",function(){
+//        showSection($(this).data("section"));
         showSection($(this).data("section"));
     });
 });
 
 function showSection(section) {
-    hideAll();  
     if(!section) {
         section = "home";
     }
     else if(section == "#" || section == ""){
         section = "home"
     }
+    
     if(!$('#'+section).is(':visible')){
-       $('#'+section).fadeIn();
+        hideAll();  
+        $('#'+section).fadeIn();
     }
+    else scrollTo(section);
     
 }
 
 function hideAll() {
-    $("section").fadeOut();
+    $("section").fadeOut().delay(900);
+}
+
+function scrollTo(section){
+  $('html, body').animate({
+      scrollTop: $("#"+section).offset().top
+  }, 800);
 }
