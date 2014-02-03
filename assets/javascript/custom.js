4df6029f41539b7a79aecec6862d7537a7305855
Jeff Quinn <jeff.quinn@gmail.com>
2014-02-02 21:12:09 -0500

    progress

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -1,19 +1,29 @@
-$(document).ready(function() {
-    var section=window.location.hash;
-    $("section").on('click',showSection(section));
+$(document).ready(function() { 
+    var section=window.location.hash.substring(1);
+    if(section !="")
+    {
+        showSection($("#"+section).closest("section").attr("id"));
+    }
+    $(".starthidden").hide();
+    $(".mynav").on("click",function(){
+        showSection($(this).data("section"));
+    });
 });
 
 function showSection(section) {
+    hideAll();  
     if(!section) {
         section = "home";
     }
     else if(section == "#" || section == ""){
         section = "home"
     }
-    hideAll();  
-    $(section).fadeIn();
+    if(!$('#'+section).is(':visible')){
+       $('#'+section).fadeIn();
+    }
+    
 }
 
 function hideAll() {
-    $("section").hide();
+    $("section").fadeOut();
 }
